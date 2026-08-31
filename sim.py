b95f404e857057449f914cca21f8924592c96a8c
miryam samad <ssamadmiryam@gmail.com>
2026-08-31 12:24:54 +0300

    first 4 planets are moving

--- sim.py
+++ sim.py
@@ -1,11 +1,10 @@
 import pygame
 import math
 
-
 pygame.init()
 
 # setting up pygame window
-width, height = 800, 800
+width, height = 1000, 800
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Miryam's Expanding Solar System Simulation")
 
@@ -16,6 +15,11 @@ blue = (100, 150, 235)
 red = (188, 39, 50)
 mustard_yellow = (225, 173, 1)
 grey = (229, 229, 229)
+white = (255, 255, 255)
+
+font = pygame.font.SysFont("comicsans", 16) # how to initialize font
+title_font = pygame.font.SysFont("Ariel", 30) # how to initialize font
+signature_font = pygame.font.SysFont("comicsans", 12) # how to initialize font
 
 
 # let's put some planets
@@ -45,7 +49,29 @@ class Planet:
     def draw(self, win): 
         x = self.x * self.SCALE + width / 2
         y = self.y * self.SCALE + height / 2
+
+        title = title_font.render("An Expanding Simulation of our Solar System", 1, white)
+        signature = signature_font.render("Amata's Creations", 1, white)
+
+        window.blit(title, (width / 3.5, 20))
+        window.blit(signature, (870, height - 30))
+
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                x, y = point
+                x = x * self.SCALE + width / 2
+                y = y * self.SCALE + height / 2
+                updated_points.append((x, y))
+
+            pygame.draw.lines(win, self.color, False, updated_points, 2) # drawing the orbit
+
         pygame.draw.circle(win, self.color, (x, y), self.radius) # how we tell our program to draw a circle
+
+        if not self.sun:
+            distance_text = font.render(self.name, 1, white)
+            window.blit(distance_text, (x - distance_text.get_width()/2, y - distance_text.get_height()/2))
+
 
     def attraction(self, other):
         other_x, other_y = other.x, other.y
@@ -67,7 +93,7 @@ class Planet:
         return x_force, y_force
 
     # make the planets move according to all forces acting upon them
-    def update_position(self, planets, velocities):
+    def update_position(self, planets):
 
         total_fx = total_fy = 0
         for planet in planets:
@@ -84,11 +110,11 @@ class Planet:
         # distance is updated (v = d / t <=> d = v * t)
         self.x += self.x_vel * self.TIMESTEP 
         self.y += self.y_vel * self.TIMESTEP
-        self.orbit.append([self.x, self.y])
+        self.orbit.append([self.x, self.y]) # adding a point to orbit list
 
 
 
-# pygame event loop, only event is moving planets here
+# pygame event loop
 def main():
     run = True
     clock = pygame.time.Clock()
@@ -98,7 +124,7 @@ def main():
 
     # planets & their velocities
     mercury = Planet(0.387*Planet.AU, 0, 12, grey, 3.30104*10**23, "Mercury")
-    mercury.y_vel = 47.4 * 1000
+    mercury.y_vel = -47.4 * 1000
 
     venus = Planet(-0.72*Planet.AU, 0, 14, mustard_yellow, 4.86732*10**24, "Venus")
     venus.y_vel = -35.02 * 1000
@@ -111,7 +137,7 @@ def main():
    
 
     planets = [sun, earth, mars, venus, mercury]
-    velocities = [0, 47.4 * 1000, -35.02 * 1000, 29.783 * 1000, 24.077 * 1000],
+
 
     while run:
         clock.tick(60) # 60 frames per second
@@ -122,7 +148,7 @@ def main():
                 run = False
 
         for planet in planets:
-            planet.update_position(planets, velocities)
+            planet.update_position(planets)
             planet.draw(window)
 
         pygame.display.update() # takes drawing actions since last update 
